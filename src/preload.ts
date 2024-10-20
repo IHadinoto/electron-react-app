efd8b7d30abbff225a5b7012eabd8eacf43eed88
Ian Nathanael Hadinoto <ianhadinoto@gmail.com>
2024-10-21 00:15:31 +1100

    Removed unused imports

--- src/preload.ts
+++ src/preload.ts
@@ -1,6 +1,5 @@
 // See the Electron documentation for details on how to use preload scripts:
 // https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
-import { EncodingType } from '@gd-react/fs/fs';
 import { contextBridge, ipcRenderer } from 'electron'
 
 contextBridge.exposeInMainWorld('electronAPI', {
